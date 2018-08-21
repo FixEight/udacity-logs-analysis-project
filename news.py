3b7542e3fe4b8d8db175d566eedf60094aafe56f
FixEight <kashifiqbal23@gmail.com>
2018-08-21 15:45:31 +0500

    Changes according to Udacity requirements

--- news.py
+++ news.py
@@ -3,17 +3,6 @@
 import psycopg2
 
 
-# Create dictionary variables to store questions and thier answers
-data_one = {}
-data_one["question"] = """
-    \n1. What are the most popular three articles of all time?"""
-data_two = {}
-data_two["question"] = """
-    \n2. Who are the most popular article authors of all time?"""
-data_three = {}
-data_three["question"] = """
-    \n3. On which days did more than 1%s of requests lead to errors?"""
-
 # Resultant queries according to the questions
 answer_one = """
     select title, num from top_articles order by top_articles.num
@@ -26,38 +15,42 @@ answer_three = """
     select to_char(date,'Mon DD, YYYY'), percentage from error_percent
     where error_percent.percentage > 1.00;"""
 
+# Global variable
+count = 1
+
 
 def run_query(user_query):
     """Return the result of the given query."""
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute(user_query)
-    result = c.fetchall()
+    data = c.fetchall()
+    global count
+    if (count < 3):
+        display(data)
+        count = count + 1
+    else:
+        display_special(data)
     db.close()
-    return result
 
 
-def print_result(result_query):
-    """Print the quesions and their related answer."""
-    print (result_query["question"])
-    for result in result_query["answer"]:
-        print ("\t" + result[0] + " ___ " + str(result[1]) + " views")
+def display(answers):
+    """Display the first two answers"""
+    for answer in answers:
+        print ("\t %s ___ %s views" % (answer[0], answer[1]))
 
 
-def print_result_special(result_query):
-    """Print the quesion number three which shows the error detail."""
-    print (result_query["question"])
-    for result in result_query["answer"]:
-        print ("\t" + str(result[0]) + " ___ " + str(result[1]) + "% errors")
-
-# Save the quries result
-data_one["answer"] = run_query(answer_one)
-data_two["answer"] = run_query(answer_two)
-data_three["answer"] = run_query(answer_three)
+def display_special(special_answer):
+    """Display the special answer"""
+    for answer in special_answer:
+        print ("\t %s ___ %s%% errors" % (answer[0], answer[1]))
 
 
-# Print the output in given format
-print_result(data_one)
-print_result(data_two)
-print_result_special(data_three)
+# Run the program and get the formatted result
+print ("\n1. What are the most popular three articles of all time?")
+run_query(answer_one)
+print ("\n2. Who are the most popular article authors of all time?")
+run_query(answer_two)
+print ("\n3. On which days did more than 1%s of requests lead to errors?")
+run_query(answer_three)
 print ("\n")
